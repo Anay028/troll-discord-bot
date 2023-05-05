a04642b62ec4bcc296cbc1c6453bde6cb9368354
Anay028 <96932471+Anay028@users.noreply.github.com>
2023-05-05 11:49:27 +0530

    added general chat

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ client.on(Events.ClientReady, async e => {
 
 client.on(Events.GuildMemberAdd, async member => {
     const trollId = member.id;
-
+    const general = await client.channels.cache.get(process.env.GENERAL_CHANNEL_ID)
     const channel = await client.channels.cache.get(process.env.CHANNEL_ID)
     const message = await channel.messages.fetch({limit: 1})
     const messageContent = message.first().content;
     const previousNumber = parseInt(messageContent.split(" ")[0].split("#")[1]);
     const currentNumber = previousNumber + 1;
     channel.send(`#${currentNumber} <@${trollId}>`)
+    general.send(`Congrats <@${trollId}> for being trolled. You are #${currentNumber}! Enjoy your day!`)
 })
 
 
